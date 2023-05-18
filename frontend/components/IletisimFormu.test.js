369556b7603d789d3705b487ad99ab44df36f063
AytacSahin <haytacsahin@gmail.com>
2023-05-19 00:02:27 +0300

    S9G4 Tamamlandı

--- frontend/components/IletisimFormu.test.js
+++ frontend/components/IletisimFormu.test.js
@@ -1,41 +1,83 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { getByPlaceholderText, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import IletisimFormu from './IletisimFormu';
 
-test('hata olmadan render ediliyor', () => {
+beforeEach(() => {
+    render(<IletisimFormu/>);
+  });
 
+// _______________________________________________🥎_______________________________________________ //
+
+test('hata olmadan render ediliyor', () => {
 });
+
+// _______________________________________________🥎_______________________________________________ //
 
 test('iletişim formu headerı render ediliyor', () => {
-
+    expect(screen.getByText(/İletişim Formu/i)).toBeInTheDocument();
 });
+
+// _______________________________________________🥎_______________________________________________ //
 
 test('kullanıcı adını 5 karakterden az girdiğinde BİR hata mesajı render ediyor.', async () => {
-
+    userEvent.type(screen.getByPlaceholderText(/İlhan/i), "aa");
+    expect(screen.getAllByTestId("error").length).toBe(1);
 });
+
+// _______________________________________________🥎_______________________________________________ //
 
 test('kullanıcı inputları doldurmadığında ÜÇ hata mesajı render ediliyor.', async () => {
-
+    userEvent.click(screen.getByRole("button", { name: /gönder/i }));
+    expect(screen.getAllByTestId("error").length).toBe(3);
 });
+
+// _______________________________________________🥎_______________________________________________ //
 
 test('kullanıcı doğru ad ve soyad girdiğinde ama email girmediğinde BİR hata mesajı render ediliyor.', async () => {
-
+    userEvent.type(screen.getByPlaceholderText(/İlhan/i), "Aytaç");
+    userEvent.type(screen.getByPlaceholderText(/Mansız/i), "Şahin");
+    userEvent.click(screen.getByRole("button", { name: /gönder/i }));
+    expect(screen.getAllByTestId("error").length).toBe(1);
 });
+
+// _______________________________________________🥎_______________________________________________ //
 
 test('geçersiz bir mail girildiğinde "email geçerli bir email adresi olmalıdır." hata mesajı render ediliyor', async () => {
-
+    userEvent.type(screen.getByPlaceholderText(/yüzyılıngolcüsü@hotmail.com/i), "aaa");
+    expect(screen.getByText(/hata: email geçerli bir email adresi olmalıdır\./i)).toBeInTheDocument();
 });
+
+// _______________________________________________🥎_______________________________________________ //
 
 test('soyad girilmeden gönderilirse "soyad gereklidir." mesajı render ediliyor', async () => {
-
+    userEvent.type(screen.getByPlaceholderText(/İlhan/i), "Aytaç");
+    userEvent.type(screen.getByPlaceholderText(/yüzyılıngolcüsü@hotmail.com/i), "aytac@aytac.com");
+    userEvent.click(screen.getByRole("button", { name: /gönder/i }));
+    screen.getByText(/hata: soyad gereklidir\./i)
 });
+
+// _______________________________________________🥎_______________________________________________ //
 
 test('ad,soyad, email render ediliyor. mesaj bölümü doldurulmadığında hata mesajı render edilmiyor.', async () => {
-
+    userEvent.type(screen.getByPlaceholderText(/İlhan/i), "Aytaç");
+    userEvent.type(screen.getByPlaceholderText(/Mansız/i), "Şahin");
+    userEvent.type(screen.getByPlaceholderText(/yüzyılıngolcüsü@hotmail.com/i), "aytac@aytac.com");
+    userEvent.click(screen.getByRole("button", { name: /gönder/i }));
+    expect(screen.queryAllByTestId("error").length).toBe(0);
 });
 
+// _______________________________________________🥎_______________________________________________ //
 test('form gönderildiğinde girilen tüm değerler render ediliyor.', async () => {
-
+    userEvent.type(screen.getByPlaceholderText(/İlhan/i), "Aytaç");
+    userEvent.type(screen.getByPlaceholderText(/Mansız/i), "Şahin");
+    userEvent.type(screen.getByPlaceholderText(/yüzyılıngolcüsü@hotmail.com/i), "aytac@aytac.com");
+    userEvent.type(screen.getByText(/mesaj/i), "Tüm bilgilerimi girdim. Gerekenin yapılmasını rica ederim.");
+    userEvent.click(screen.getByRole("button", { name: /gönder/i }));
+    expect(screen.queryByTestId("firstnameDisplay")).toHaveTextContent("Aytaç");
+    expect(screen.queryByTestId("lastnameDisplay")).toHaveTextContent("Şahin");
+    expect(screen.queryByTestId("emailDisplay")).toHaveTextContent("aytac@aytac.com");
+    expect(screen.queryByTestId("messageDisplay")).toHaveTextContent("Tüm bilgilerimi girdim. Gerekenin yapılmasını rica ederim.");
 });
+
---
+++ frontend/components/Test-Senaryoları.md
@@ -0,0 +1,7 @@
+## Uygulama İçin Yazılabilecek Test Senaryoları:
+
+- İletişim Formu isimli bir <h1> tag'i olup olmadığı kontrol edilebilir.
+- Toplamda 4 adet input box olup olmadığı kontrol edilebilir.
+- Ad* bölümüne 4 harfli bir kelime gönderilip, sayfada "Hata: ad en az 5 karakter olmalıdır." mesajı alınıp alınmadığı kontrol edilebilir.
+- Email* bölümüne geçersiz formatta bir mail adresi gönderilip, sayfada "Hata: email geçerli bir email adresi olmalıdır." mesajı alınıp alınmadığı kontrol edilebilir.
+- Tüm input'lara doğru veriler gönderilerek, "GÖNDER" butonuna tıklandığında, data-testid'leri firstnameDisplay, lastnameDisplay, emailDisplay ve messageDisplay olan <p> taglerinin görünüp görünmediği kontrol edilebilir.
